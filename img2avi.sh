85aaf5b33c0f4f79f5b59d34db5089282f46fe26
Mario Aeby <spam@eMeidi.com>
2013-05-26 23:14:00 +0200

    Reordered program flow

--- img2avi.sh
+++ img2avi.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 
-SCRIPTROOT=`dirname $0`
+if [ $# -lt 1 ]
+then
+	echo "Usage: $0 <directory>"
+	exit 1
+fi
 
+# Default settings are used when no configuration file is presen
 MENCODER=`which mencoder`
 IMAGEEXTENSION="jpg"
 FRAMERATE=20
 VCODEC="mpeg4"
-VBITRATE="1800"
+VBITRATE=1800
 
+AVIMOVIE="movie.avi"
+if [ $# -eq 2 ]
+then
+	AVIMOVIE="$2.avi"
+	echo "Using custom set video name '$AVIMOVIE'"
+fi
+
+SCRIPTROOT=`dirname $0`
 CONFIG="$SCRIPTROOT/wetterradar.cfg"
 if [ -f "$CONFIG" ]
 then
 	echo "Sourcing configuration from '$CONFIG'"
 	source $CONFIG
-fi
-
-if [ $# -lt 1 ]
-then
-	echo "Usage: $0 <directory>"
-	exit 1
 fi
 
 WORKINGDIR="$1"
@@ -39,8 +46,13 @@ echo "Switching to directory '$WORKINGDIR'"
 cd "$WORKINGDIR"
 
 AVIMOVIE="movie.avi"
+if [ $# -eq 2 ]
+then
+	AVIMOVIE="$2.avi"
+	echo "Using custom set video name '$AVIMOVIE'"
+fi
 
-echo "Encoding all JPEG files present in directory '$WORKINGDIR' into an avimovie"
+echo "Encoding all JPEG files present in directory '$WORKINGDIR' into an avi movie"
 $MENCODER "mf://*.$IMAGEEXTENSION" -really-quiet -mf fps=$FRAMERATE -o "$AVIMOVIE" -ovc lavc -lavcopts vcodec=$VCODEC:vbitrate=$VBITRATE 2>/dev/null
 
 exit 0
